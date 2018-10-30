1a9e68fb75b43bcec565d6251d62f564076ff03b
Hưng Lê <hunglm@vzota.com.vn>
2018-10-31 01:35:41 +0700

    Test vue

---
+++ precache-manifest.6a21173e7253bef783e5d794b6876472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ca407450bab8fbef02ec",
+    "url": "/js/chunk-vendors.a3d6e407.js"
+  },
+  {
+    "revision": "ec5427c2ebb9945e5f85",
+    "url": "/js/app.5e916905.js"
+  },
+  {
+    "revision": "51482a80ab1c843a4a7d",
+    "url": "/js/about.9a4e0480.js"
+  },
+  {
+    "revision": "884998caa866a564ec85f79cb4e06c9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ec5427c2ebb9945e5f85",
+    "url": "/css/app.ae4ddba3.css"
+  }
+];
